0ca47a3546a96ce368115c478f293c299fe03873
Lilit <lilithakobyan1994@mail.ru>
2017-02-27 01:07:04 +0400

    add ArrComp

---
+++ Tasks/ArrComp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrComp
+{
+    class Program
+    {
+        public static bool Contains(int[] arr, int item)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]==item)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool Compare(int[] a, int[] b)
+        {
+            if (a.Length == b.Length)
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (Contains(b, a[i]))
+                    {
+                        continue;
+                    }
+                    else { return false;}
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            int[] arr1 = {1, 2, 3, 5, 8};
+            int[] arr2 = {2, 1, 5, 8, 7};
+            Console.WriteLine(Compare(arr1,arr2));
+            Console.ReadKey();
+        }
+    }
+}
