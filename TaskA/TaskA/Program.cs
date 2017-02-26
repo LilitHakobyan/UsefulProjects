356e270fca8a96aeb611c8631a6e18c66ba9f74f
Lilit <lilithakobyan1994@mail.ru>
2017-02-26 17:18:46 +0400

    add taskA

---
+++ TaskA/TaskA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskA
+{
+    class Program
+    {
+        public static void DivFT(int a)
+        {
+            if ((a % 5 == 0) && (a % 3 == 0))
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else if (a % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (a % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
