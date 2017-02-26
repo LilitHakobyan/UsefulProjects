77761a65cdd5ec8f0978aeb4a22c734ba2bbea89
Lilit <lilithakobyan1994@mail.ru>
2017-02-26 18:42:32 +0400

    add B

--- Tasks/TaskB/Program.cs
+++ Tasks/TaskB/Program.cs
@@ -10,8 +10,24 @@ namespace TaskB
     {
         static void Main(string[] args)
         {
-
-
+            int m=0;
+            int k=0; 
+          int n = Convert.ToInt32(Console.ReadLine());
+            while (n!=0)
+            {
+                m = n%10;
+                if ((m==0)|| (m == 4) || (m == 6) || (m == 9))
+                {
+                    k++;
+                }
+                else if (m==8)
+                {
+                    k += 2;
+                }
+                n = n/10;
+            }
+            Console.WriteLine(k);
+            Console.ReadKey();
 
         }
     }
