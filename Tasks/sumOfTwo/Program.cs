f829b0c803df1b7a69e47ddb15634086fa978d8c
Lilit <lilithakobyan1994@mail.ru>
2017-03-07 23:17:00 +0400

    add sumOfTwo

---
+++ Tasks/sumOfTwo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace sumOfTwo
+{
+    class Program
+    {
+      public static  bool sumOfTwo(int[] a, int[] b, int v)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if (a[i] + b[j] == v)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = {1, 2, 3};
+            int[] b = {10, 20, 30, 40};
+            sumOfTwo(a, b, 42);
+
+        }
+    }
+    }
+
