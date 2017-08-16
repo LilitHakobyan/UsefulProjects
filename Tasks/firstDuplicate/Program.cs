11af0cafe36c7ebf1c4de8334d5f174880b46202
lilithakobyan <lilithakobyan1994@mail.ru>
2017-08-16 20:46:30 +0400

    FirstDuplicate

---
+++ Tasks/firstDuplicate/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstDuplicate
+{
+    class Program
+    { public static int firstDuplicatebyStack(int[] a)
+        {
+            var stack = new Stack<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (stack.Contains(a[i]))
+                    return a[i]; 
+                stack.Push(a[i]);
+            }
+            return -1;
+        }
+
+        public static int FirstDuplicatebyDictionary(int[] a)// faster than firstDuplicatebyStack
+        {
+            var dictionary = new Dictionary<int,int>();
+            foreach (var t in a)
+            {
+                try
+                {
+                    dictionary.Add(t, t);
+
+                }
+                catch 
+                {
+                    return t;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = {2, 3, 3, 4, 5, 6, 2};
+             Console.WriteLine(firstDuplicatebyStack(a));
+            Console.WriteLine(FirstDuplicatebyDictionary(a));
+
+            Console.ReadKey();
+        }
+    }
+}
