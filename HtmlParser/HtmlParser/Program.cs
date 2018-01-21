694b7c6eeeb9b63348630016476c5cbb54478063
lilithakobyan <lilithakobyan1994@mail.ru>
2018-01-21 20:35:27 +0400

    changes

---
+++ HtmlParser/HtmlParser/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace HtmlParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string htmlCode;
+            using (var client = new WebClient())
+            {
+                htmlCode = client.DownloadString("https://free-proxy-list.net/");
+            }
+            var tdLlist = new List<string>();
+            //Console.WriteLine(htmlCode);
+            for (int i = 0; i < htmlCode.Length; i++)
+            {
+                if ((htmlCode[i] == '<') && (htmlCode[i + 1] == 't') && (htmlCode[i + 2] == 'd') && (htmlCode[i + 3] == '>'))
+                {
+                    int index = i + 4;
+                    string element = string.Empty;
+                    while (htmlCode[index] != '<')
+                    {
+                        element += htmlCode[index++];
+
+                    }
+                    tdLlist.Add(element);
+                }
+            }
+
+            var ipList = new List<string>();
+            var portList = new List<int>();
+            var countryList = new List<string>();
+            for (int i = 0; i < tdLlist.Count; i += 4)
+            {
+                ipList.Add(tdLlist[i]);
+                portList.Add(int.Parse(tdLlist[i + 1]));
+                countryList.Add(tdLlist[i + 2]);
+            }
+
+         
+        }
+    }
+}
---
+++ OOPImpementationSimple/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorphismTest
+{
+    public class A
+    {
+        public virtual void DoWork()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    public class B : A
+    {
+        public override void DoWork()
+        {
+            Console.WriteLine("B");
+        }
+    }
+    public class C : B
+    {
+        public sealed override void DoWork()
+        {
+            Console.WriteLine("C");
+        }
+    }
+    public class D : C
+    {
+        public new void DoWork()
+        {
+            Console.WriteLine("D");
+        }
+    }
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            D d = new D();
+            Console.WriteLine(d.GetType());
+            d.DoWork();
+
+            C c = d;
+            Console.WriteLine(c.GetType());
+            c.DoWork();
+
+            B b = c;
+            Console.WriteLine(b.GetType());
+            b.DoWork();
+
+            A a = b;
+            Console.WriteLine(a.GetType());
+            a.DoWork();
+
+            //A a1 = new A();
+            //a1.DoWork();
+            //B b1 = new B();
+            //b1.DoWork();
+            //A a2 = b1;
+            //a2.DoWork();
+            Console.ReadKey();
+        }
+    }
+}
